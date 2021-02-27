e8e7419be91135528f4e7f8e7c7819895e311130
lbacon17 <louis.bacon17@icloud.com>
2021-02-27 14:16:49 +0000

    implemented search and filtering functionality in search bar

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
 
 from .models import Product, Category
 
@@ -12,9 +15,37 @@ def shop_all(request):
     sort = None
     direction = None
 
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey == 'lower_name'
+                shop_items = shop_items.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            shop_items = shop_items.order_by(sortkey)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search terms!")
+                return redirect(reverse('shop_all'))
+            
+            queries = Q(name__icontains=query)
+            shop_items = shop_items.filter(queries)
+
+    sort_by = f'{sort}_{direction}'
+
     context = {
         'shop_items': shop_items,
+        'search_term': query,
         'categories': categories,
+        'sort_by': sort_by,
     }
 
     return render(request, 'shop/shop.html', context)
