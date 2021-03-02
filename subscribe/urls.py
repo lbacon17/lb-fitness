8c44d7443d10f9e6b8ca89978c68d5f38f18741d
lbacon17 <louis.bacon17@icloud.com>
2021-03-02 19:51:50 +0000

    created view to get subscription plan user selects and take user through to checkout

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.subscribe_page, name='subscribe_page'),
+    path('subscribe/<int:package_id>', views.get_subscription, name='get_subscription'),
 ]
--- subscribe/views.py
+++ subscribe/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponse
+from django.contrib.auth.decorators import login_required
 
 from .models import Package
 
 
 def subscribe_page(request):
+    """This view renders the different subscription packages a user can buy."""
     packages = Package.objects.all()
     context = {
         'packages': packages,
     }
     return render(request, 'subscribe/subscribe.html', context)
+
+
+@login_required
+def get_subscription(request, package_id):
+    """This view takes the user to a checkout page where the amount paid depends"""
+    """on the subscription plan chosen"""
+    package = get_object_or_404(Package, pk=package_id)
+    if not request.user.is_authenticated:
+        messages.error(request, 'You must create an account to subscribe.')
+        return redirect(url, 'account_signup')
+    else:
+        context = {
+            'package': package,
+        }
+        return render(request, 'subscribe/get_subscription.html', context)
