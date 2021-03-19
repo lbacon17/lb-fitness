d74eca1a9a08092cf2d66719b1e85725e65d4d60
lbacon17 <louis.bacon17@icloud.com>
2021-03-19 23:50:06 +0000

    registered storeuser model so default user info is visible in admin panel

--- user_profiles/admin.py
+++ user_profiles/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import StoreUser
 
-# Register your models here.
+
+class StoreUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'default_email_address',
+        'default_phone_number',
+        'default_address_line1',
+        'default_address_line2',
+        'default_town_or_city',
+        'default_county_or_region',
+        'default_postcode',
+        'default_country',
+    )
+
+    ordering = ('user',)
+
+
+admin.site.register(StoreUser, StoreUserAdmin)
