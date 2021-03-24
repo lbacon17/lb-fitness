2073eba12f6c918a541b828dc3cc63dd28966d78
lbacon17 <louis.bacon17@icloud.com>
2021-03-24 20:06:08 +0000

    imported path and env to settings file

--- lb_fitness/settings.py
+++ lb_fitness/settings.py
@@ -12,7 +12,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 import dj_database_url
+from os import path
 from pathlib import Path
+
+if path.exists('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
