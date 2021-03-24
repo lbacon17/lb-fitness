ab7f55805fd8f7a263f8fafe24b33a9b18a3a120
lbacon17 <louis.bacon17@icloud.com>
2021-03-24 18:26:44 +0000

    updated contact app to save contact request to admin panel with the ability for admin to mark a request as answered

--- contact/views.py
+++ contact/views.py
@@ -1,16 +1,20 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 
 from .forms import ContactForm
+from .models import Contact
 
 
 def contact(request):
+    contact_request = None
     if request.method == 'POST':
         contact_form = ContactForm(request.POST)
         if contact_form.is_valid():
-            contact_form.save()
+            contact_request = contact_form.save(commit=False)
+            contact_request.user = request.user
+            contact_request.save()
             messages.success(request, 'Your query was submitted successfully')
             return redirect(reverse('contact'))
         else:
