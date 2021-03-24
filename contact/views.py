3fafe8ce1420a18a1942fb4661fa71ce35991a5b
lbacon17 <louis.bacon17@icloud.com>
2021-03-24 18:40:45 +0000

    ran migrations after connecting postgres db

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
