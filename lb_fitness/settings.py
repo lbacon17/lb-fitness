9f362937805beb76256f53007f10224de09736cd
lbacon17 <louis.bacon17@icloud.com>
2021-03-24 12:30:16 +0000

    updated staticfiles_storage variable in settings as heroku not currently recognising installed storages module

--- lb_fitness/settings.py
+++ lb_fitness/settings.py
@@ -197,7 +197,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
