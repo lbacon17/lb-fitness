2d9c94a100fd800d2be8d662ed0f89343d2f0b46
lbacon17 <louis.bacon17@icloud.com>
2021-03-24 16:39:31 +0000

    updated stripe elements file in checkout app and added link to stripe elements subscription file on subscribe template

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,3 +1,4 @@
+// Renders card element on page
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
@@ -33,9 +34,9 @@ card.addEventListener('change', function(event) {
 });
 
 // Handles form submission
-var subscriptionForm = document.getElementById('payment-form');
+var paymentForm = document.getElementById('payment-form');
 
-subscriptionForm.addEventListener(function (ev) {
+paymentForm.addEventListener(function (ev) {
     ev.PreventDefault();
     card.update({'disabled': true});
     $('#complete-order').attr('disabled', true);
@@ -53,16 +54,16 @@ subscriptionForm.addEventListener(function (ev) {
             payment_method: {
                 card: card,
                 customer_info: {
-                    full_name: $.trim(subscriptionForm.full_name.value),
-                    email_address: $.trim(subscriptionForm.email_address.value),
-                    phone_number: $.trim(subscriptionForm.phone_number.value),
+                    full_name: $.trim(paymentForm.full_name.value),
+                    email_address: $.trim(paymentForm.email_address.value),
+                    phone_number: $.trim(paymentForm.phone_number.value),
                     address: {
-                        address_line1: $.trim(subscriptionForm.address_line1.value),
-                        address_line2: $.trim(subscriptionForm.address_line2.value),
-                        town_or_city: $.trim(subscriptionForm.town_or_city.value),
-                        county_or_region: $.trim(subscriptionForm.county_or_region.value),
-                        postcode: $.trim(subscriptionForm.postcode.value),
-                        country: $.trim(subscriptionForm.country.value)
+                        address_line1: $.trim(paymentForm.address_line1.value),
+                        address_line2: $.trim(paymentForm.address_line2.value),
+                        town_or_city: $.trim(paymentForm.town_or_city.value),
+                        county_or_region: $.trim(paymentForm.county_or_region.value),
+                        postcode: $.trim(paymentForm.postcode.value),
+                        country: $.trim(paymentForm.country.value)
                     }
                 }
             }
@@ -71,10 +72,10 @@ subscriptionForm.addEventListener(function (ev) {
                 var errorElement = document.getElementById('card-errors');
                 $(errorElement).html(`<span>${result.error.message}</span>`);
                 card.update({'disabled': false});
-                $('#complete-subscription').attr('disabled', false);
+                $('#complete-order').attr('disabled', false);
             } else {
                 if (result.paymentIntent.status === 'succeeded') {
-                    subscriptionForm.submit();
+                    paymentForm.submit();
                 }
             }
         });
--- subscribe/static/subscribe/js/stripe_elements.js
+++ subscribe/static/subscribe/js/stripe_elements.js
@@ -1,7 +1,3 @@
-// Logic, templating and CSS recommendations taken from Stripe documentation:
-// https://stripe.com/docs/payments/accept-a-payment
-// https://stripe.com/docs/stripe-js
-
 // Renders card element on page
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
